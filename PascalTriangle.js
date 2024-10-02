48fb8c3f452d54e50a67311beec9d7d133646822
Ali-Azhar128 <68686318+Ali-Azhar128@users.noreply.github.com>
2024-10-02 13:45:12 +0500

    Removed unnecessary code

--- PascalTriangle.js
+++ PascalTriangle.js
@@ -29,37 +29,3 @@ const PascalTriangle = (n) => {
 
 
 console.log(PascalTriangle(2))
-
-
-
-
-
-// let arr = [[1], [1, 1]]
-// const PascalTriangle = (n) => {
-//     if(n === 0){
-//         return arr[0]
-//     }else if(n === 1){
-//         return arr[1]
-//     }
-//     let newArr = [1]
-//    for(let i = 2; i <= n; i++){
-//         for(let j = 0; j <= i; j++){
-//             if(j === 0){
-//                 newArr[j] = 1
-//             }
-//             if(j === i){
-//                 newArr[j] = 1
-//                 break
-//             }
-//             newArr[j + 1] = arr[i - 1][j] + arr[i - 1][j + 1]
-            
-            
-//         }
-//         arr.push(newArr)
-//         newArr = []
-//    }
-//    return arr[n]
-// }
-
-
-// console.log(PascalTriangle(4))
